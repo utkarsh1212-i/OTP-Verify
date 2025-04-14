7ab4bd8dd5886aeabd109c05268820232bfbd0c6
Utkarsh BAiragi <utkarshui1212@gmail.com>
2025-04-14 12:27:05 +0530

    next js client-next folder removed

--- react-login-app/client-next/pages/_app.js
+++
@@ -1,9 +0,0 @@
-import '../styles/index.css'; // Import global CSS here
-
-import '../styles/StudentloginForm.css';
-import '../styles/OTPVerification.css';
-import '../styles/admindashboard.css'
-
-export default function App({ Component, pageProps }) {
-    return <Component {...pageProps} />;
-}
