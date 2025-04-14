8b34b0fbd49e24781fdf17227244991a17d64d99
Utkarsh BAiragi <utkarshui1212@gmail.com>
2025-04-14 16:19:18 +0530

    frontend- vercel domain changes for backend

--- react-login-app/client/src/api/axios.js
+++ react-login-app/client/src/api/axios.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const instance = axios.create({
-    baseURL: 'http://localhost:5000', // Server URL
+    baseURL: 'https://otp-verify-backend.vercel.app', // Server URL
 });
 
 export default instance;
